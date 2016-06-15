43cf6f90a5beb9bef614788a20c4eff5ce05b3f7
toadordragon <heymrspaceman@users.noreply.github.com>
2016-06-15 23:45:25 +0100

    Added more tests

--- BoostrapperChecker/BootstrapperCheckerTests/BuildProjectTests.cs
+++ BoostrapperChecker/BootstrapperCheckerTests/BuildProjectTests.cs
@@ -9,12 +9,44 @@ namespace BootstrapperCheckerTests
     public class BuildProjectTests
     {
         [TestMethod]
+        public void MissingDependenciesAndArtifacts()
+        {
+            var mockXmlLoader = new Mock<IXmlLoader>();
+            mockXmlLoader.Setup(x => x.LoadFromFile("test")).Returns(
+                @"<project name=""DummyProject"" default=""all"">" +
+                @"</project>");
+
+            BuildProject testProject = new BuildProject("TestProject", mockXmlLoader.Object);
+            testProject.Load("test");
+
+            Assert.AreEqual(false, testProject.IsDependenciesPresent);
+            Assert.AreEqual(false, testProject.IsArtifactPresent);
+            Assert.AreEqual(false, testProject.IsOutputArtifactValid);
+        }
+
+        [TestMethod]
+        public void IncorrectFinalArtifactName()
+        {
+            var mockXmlLoader = new Mock<IXmlLoader>();
+            mockXmlLoader.Setup(x => x.LoadFromFile("test")).Returns(
+                @"<project name=""DummyProject"" default=""all"">" +
+                @"<property name=""dependencies"" value=""Project1Output.txt, Project2Output.txt, Project2Output.txt""/>" +
+                @"<property name=""finalOutputArtifact"" value=""WrongProjectOutput.txt""/>" +
+                @"</project>");
+
+            BuildProject testProject = new BuildProject("TestProject", mockXmlLoader.Object);
+            testProject.Load("test");
+
+            Assert.AreEqual(false, testProject.IsOutputArtifactValid);
+        }
+
+        [TestMethod]
         public void DuplicateDependencies()
         {
             var mockXmlLoader = new Mock<IXmlLoader>();
             mockXmlLoader.Setup(x => x.LoadFromFile("test")).Returns(
-                @"<project name=""DummyProject"" default=""all"">" + 
-                @"<property name=""dependencies"" value=""Project1Output.txt, Project2Output.txt, Project2Output.txt""/>" + 
+                @"<project name=""DummyProject"" default=""all"">" +
+                @"<property name=""dependencies"" value=""Project1Output.txt, Project2Output.txt, Project2Output.txt""/>" +
                 @"<property name=""finalOutputArtifact"" value=""DummyProjectOutput.txt""/>" +
                 @"</project>");
 
@@ -23,7 +55,6 @@ namespace BootstrapperCheckerTests
 
             Assert.AreEqual(true, testProject.IsDependenciesPresent);
             Assert.AreEqual(true, testProject.DuplicateDependencies);
-
         }
     }
 }
