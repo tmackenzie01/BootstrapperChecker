b5463e8bebcf53529d08a46218987dd693759973
tmackenzie01 <thomas.mackenzie@vmsuk.com>
2016-06-16 10:56:47 +0100

    Added tests for bootstrapper dependencies sorting

--- BoostrapperChecker/BootstrapperCheckerTests/BuildProjectTests.cs
+++ BoostrapperChecker/BootstrapperCheckerTests/BuildProjectTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using BoostrapperChecker;
+using System.Collections.Generic;
 
 namespace BootstrapperCheckerTests
 {
@@ -92,6 +93,27 @@ namespace BootstrapperCheckerTests
             Assert.AreEqual(true, testProject.IsDependenciesPresent);
             Assert.AreEqual(true, testProject.DuplicateDependencies);
             Assert.AreEqual(true, testProject.DependenciesCorrectOrder);
+        }
+
+        [TestMethod]
+        public void SortedDependencies()
+        {
+            var mockXmlLoader = new Mock<IXmlLoader>();
+            mockXmlLoader.Setup(x => x.LoadFromFile("test")).Returns(
+                @"<project name=""DummyProject"" default=""all"">" +
+                @"<property name=""dependencies"" value=""Project1Output.txt,Project2Output.txt,Project3Output.txt,DependenciesOutput.txt,InstallsOutput.txt""/>" +
+                @"<property name=""finalOutputArtifact"" value=""DummyProjectOutput.txt""/>" +
+                @"</project>");
+
+            BuildProject testProject = new BuildProject("TestProject", mockXmlLoader.Object);
+            testProject.Load("test");
+
+            List<ProjectTitle> dependenciesInCorrectOrder = new List<ProjectTitle>() { new ProjectTitle("Dependencies"),
+                                                                                        new ProjectTitle("Installs"),
+                                                                                        new ProjectTitle("Project1"),
+                                                                                        new ProjectTitle("Project2"),
+                                                                                        new ProjectTitle("Project3") };
+            CollectionAssert.AreEquivalent(testProject.Dependencies, dependenciesInCorrectOrder);
         }
     }
 }
--- BoostrapperChecker/BootstrapperCheckerTests/ProjectTitleTests.cs
+++ BoostrapperChecker/BootstrapperCheckerTests/ProjectTitleTests.cs
@@ -15,7 +15,7 @@ namespace BootstrapperCheckerTests
             ProjectTitle project1 = new ProjectTitle("Project");
             ProjectTitle project2 = new ProjectTitle("Project");
             ProjectTitle project3 = new ProjectTitle("ProjectDifferent");
-            
+
             Assert.AreEqual(true, project1.Equals(project2));
             Assert.AreEqual(false, project1.Equals(project3));
         }
@@ -38,6 +38,30 @@ namespace BootstrapperCheckerTests
 
             Assert.AreEqual(true, project1.CorrectSpacing);
             Assert.AreEqual(false, project2.CorrectSpacing);
+        }
+
+        [TestMethod]
+        public void ProjectTitlesComparing()
+        {
+            ProjectTitle projectDependencies1 = new ProjectTitle("Dependencies");
+            ProjectTitle projectDependencies2 = new ProjectTitle("Dependencies");
+            ProjectTitle projectInstalls = new ProjectTitle("Installs");
+            ProjectTitle project1 = new ProjectTitle("Project1");
+            ProjectTitle project2 = new ProjectTitle("Project2");
+            ProjectTitle project3 = new ProjectTitle("Project3");
+
+            // Same projects (2 different instances of the dependencies one though)
+            List<ProjectTitle> sameProjects1 = new List<ProjectTitle>() { project1, projectDependencies1, project2, projectInstalls, project3 };
+            List<ProjectTitle> sameProjects2 = new List<ProjectTitle>() { project1, projectDependencies2, project2, projectInstalls, project3 };
+            CollectionAssert.AreEqual(sameProjects1, sameProjects2);
+
+            // Same projects - after sorting (2 different instances of the dependencies one though)
+            List<ProjectTitle> allProjects = new List<ProjectTitle>() { project1, projectDependencies1, project2, projectInstalls, project3 };
+            List<ProjectTitle> allProjectsInCorrectOrder = new List<ProjectTitle>() { projectDependencies2, projectInstalls, project1, project2, project3 };
+            ProjectTitleComparer comp = new ProjectTitleComparer();
+            allProjects.Sort(comp);
+
+            CollectionAssert.AreEqual(allProjects, allProjectsInCorrectOrder);
         }
     }
 }
